54d9fa3fe9dcf06b7a2afa3cc89815e87c8168f4
jiang-123456-cpu <2509500763@qq.com>
2026-07-11 22:24:48 +0800

    Update vector_service.py

--- fastapi-learning/services/vector_service.py
+++ fastapi-learning/services/vector_service.py
@@ -1,7 +1,8 @@
 from utils.config_handler import chroma_conf
 from model.factory import embed_model
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-
+from langchain_community.retrievers import BM25Retriever
+from langchain_classic.retrievers import EnsembleRetriever
 from langchain_chroma import Chroma
 from langchain_core.documents import Document
 import os
@@ -45,9 +46,9 @@ class VectorStore:
             embedding_function=embed_model,
             persist_directory=chroma_conf["persist_directory"]
         )
-        return vectorstore.as_retriever(
-            search_kwargs={'k': chroma_conf["k"]}
-        )
+        vector_retriever=vectorstore.as_retriever( search_kwargs={"k": 2})
+        bm25_retriever=BM25Retriever.from_texts(vectorstore.get())
+        return EnsembleRetriever(retrievers=[vector_retriever, bm25_retriever], weights=[0.5, 0.5])
 
     def load_document(self,file_path,file_type,kb_id,doc_id):
           # 将扩展名补上点号，确保 endswith 能正确匹配
@@ -61,6 +62,12 @@ class VectorStore:
           if check_md5_hex(md5_str):
             raise Exception("文件已存在")
           documents = get_file_documents(file_path)
+          print(f"DEBUG: 解析出的文档数量 = {len(documents)}")
+          if len(documents) > 0:
+              print(f"DEBUG: 第一个文档内容预览 = {documents[0].page_content[:200]}...")
+          else:
+              # 直接抛出自定义异常，便于区分
+              raise Exception(f"文件解析失败，未能提取到任何文本。文件类型：{file_path.split('.')[-1]}")
           if not documents:
             raise Exception("文件为空")
           chunks = self.spliter.split_documents(documents)
