c6dc080ce3250b15000af51d3991ebbfdd413c4f
jiang-123456-cpu <2509500763@qq.com>
2026-08-15 10:58:37 +0800

    Delete fastapi-learning directory

--- fastapi-learning/scripts/check_hf_connect.py
+++
@@ -1,8 +0,0 @@
-import urllib.request, sys
-url = 'https://huggingface.co'
-try:
-    with urllib.request.urlopen(url, timeout=5) as r:
-        print('OK', r.status)
-except Exception as e:
-    print('ERR', repr(e))
-    sys.exit(2)
--- fastapi-learning/scripts/try_import.py
+++
@@ -1,6 +0,0 @@
-import traceback
-try:
-    import services.recorder_service as r
-    print('IMPORT_OK')
-except Exception:
-    traceback.print_exc()
